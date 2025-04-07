61bba39d08edb825cfb91554529c15b8db97aee8
Nikita <nikitalomieiko@gmail.com>
2025-04-07 15:32:25 +0300

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const path = require('path');
 
 
-const db = path.join(__dirname, "./server/json/db.json");
+const db = path.join(__dirname, "db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
@@ -15,6 +15,7 @@ const port = 8080;
 
 module.exports = (req, res) => {
   res.json({ message: "Hello from Vercel Serverless!!!!" });
+  
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
