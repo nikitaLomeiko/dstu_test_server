2b52bd1e1cd6d23eeee723f5ba516f9a8b3aad89
Nikita <nikitalomieiko@gmail.com>
2025-04-07 14:55:01 +0300

    init expess server & integration with json-server api

--- README.md
+++
@@ -1,1 +0,0 @@
-# dstu_test_server
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const jsonServer = require("json-server");
+const path = require('path');
+
+
+const db = path.join(__dirname, "./server/json/db.json");
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
+
+
+const app = express();
+app.use(middlewares);
+app.use("/api", router);
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
