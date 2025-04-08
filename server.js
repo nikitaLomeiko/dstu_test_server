015146b477950b04ff9415ac29e448dccf4e8167
Nikita <nikitalomieiko@gmail.com>
2025-04-08 12:28:48 +0300

    fix tmp

--- server.js
+++ server.js
@@ -3,25 +3,8 @@ const jsonServer = require("json-server");
 const path = require("path");
 const fs = require("fs");
 
-// Путь к БД — всегда /tmp/db.json в serverless-среде
-const dbPath = process.env.IS_SERVERLESS
-  ? path.join("/tmp", "db.json")
-  : path.join(__dirname, "db.json");
+const dbPath = path.join("./tmp", "db.json")
 
-// Если это serverless-среда, гарантируем, что db.json есть в /tmp
-if (process.env.IS_SERVERLESS) {
-  try {
-    // Пытаемся скопировать db.json из проекта в /tmp
-    fs.copyFileSync(path.join(__dirname, "db.json"), dbPath);
-    console.log('Файл данных успешно скопирован в ', dbPath)
-  } catch (err) {
-    // Если файла нет (например, при первом запуске), создаём пустой
-    fs.writeFileSync(dbPath, JSON.stringify({ posts: [], users: [] }));
-    console.log('Файл данных не найден. Создан новый файл')
-  }
-}
-
-// Инициализируем json-server с указанием пути к /tmp/db.json
 const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
